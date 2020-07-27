a4a529c22198b45cf7046a5d08e7b9a9f6565981
think-next <2493393471@qq.com>
2020-07-27 08:10:23 +0800

    update

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import "sync"
+
+var (
+	name = "neojos"
+)
+
+type Person struct {
+	Name string `json:"name"`
+}
+
+// @Summary 函数的总结
+// @Desc 函数的描述
+func main() {
+
+	// 总结：
+	//
+	sync.Mutex{}
+}
