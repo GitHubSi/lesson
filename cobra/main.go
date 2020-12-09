05eff51d2348c9cc3423d1352189b562fcd02ed8
fuhui <fuhui@tal.com>
2020-12-09 21:27:28 +0800

    office

--- cobra/main.go
+++ cobra/main.go
@@ -1,7 +1,26 @@
 package main
 
-import "github.com/think-next/lesson/cobra/cmd"
+import (
+	"fmt"
+	"github.com/spf13/cast"
+	"reflect"
+)
 
 func main() {
-	cmd.Execute()
+	//cmd.Execute()
+
+	type Infos []string
+
+	personA := Infos{"fuhui"}
+	paramsKv := reflect.ValueOf(personA)
+
+	paramIndexV := paramsKv.Index(0)
+	fmt.Println(paramIndexV)
+
+	paramIndexVal := reflect.ValueOf(paramIndexV.Interface())
+	name := cast.ToString(paramIndexVal.Interface())
+	fmt.Println(name)
+
+	name2 := cast.ToString(paramIndexV.Interface())
+	fmt.Println(name2)
 }
--- map/map_grabage.go
+++ map/map_grabage.go
@@ -1,1 +1,7 @@
 package main
+
+import "testing"
+
+func TestMap(t *testing.T) {
+
+}
