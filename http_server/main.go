665375964f670d88fe8641fe6df61c46c94553a1
think-next <2493393471@qq.com>
2020-12-14 23:26:43 +0800

    Gin Group Use

--- http_server/main.go
+++ http_server/main.go
@@ -10,9 +10,14 @@ func main() {
 	r.RedirectTrailingSlash = false
 
 	// 最基础的 GET 请求
-	r.GET("/hello", func(c *gin.Context) {
-		c.String(http.StatusOK, "%s", "hello world")
+	res := r.GET("/hello", func(c *gin.Context) {
+		c.String(http.StatusOK, "%s", "hello")
 	})
 
+	// 在上述返回 res 的基础上，继续注册，其实注册的路径还是 /word
+	// 而不是 /hello/word
+	res.GET("/word", func(c *gin.Context) {
+		c.Writer.WriteString("word")
+	})
 	r.Run(":8081")
 }
